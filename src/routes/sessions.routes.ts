18459c3ce68a1ff03459fd941b7724af53bff8bd
antonio-estevam <jhonnyimmbe@gmail.com>
2021-11-12 15:01:43 -0300

    criação de autenticação

---
+++ src/routes/sessions.routes.ts
@@ -0,0 +1,25 @@
+import {  Router } from 'express';
+import AuthenticateUserService from '../services/AuthenticateUserService';
+
+const sessionsRoutes = Router();
+
+sessionsRoutes.post('/', async (request, response) =>{
+  try {
+    const { email, password } = request.body;
+
+    const autenticateUser = new AuthenticateUserService();
+
+    const { user, token } = await autenticateUser.execute({
+      email,
+      password
+    });
+
+    //delete user.password;
+
+    return response.json({user, token});
+  } catch (error:any) {
+    return response.status(400).json({ error: error.message })
+  }
+});
+
+export default sessionsRoutes;
---
+++ src/services/AuthenticateUserService.ts
@@ -0,0 +1,45 @@
+import { getRepository } from 'typeorm';
+import { compare } from 'bcryptjs'
+import User from '../models/User';
+import { sign } from 'jsonwebtoken';
+import authCongig from '../config/auth'
+
+interface Request {
+  email: string;
+  password: string;
+}
+interface Response{
+  user: User;
+  token:string;
+}
+
+class AuthenticateUserService {
+  public async execute({ email, password}: Request): Promise<Response>{
+    const usersRepository = getRepository(User);
+
+    const user = await usersRepository.findOne({ where:{ email } });
+
+    if(!user){
+      throw new Error('Incorrect email/password combination.');
+    }
+    const passwordMatched = await compare(password, user.password);
+
+    if(!passwordMatched){
+      throw new Error('Incorrect email/password combination.');
+    }
+
+    const { secret, expiresIn} = authCongig.jwt;
+
+    const token = sign({  }, secret,{
+      subject: user.id,
+      expiresIn:secret,
+    });
+
+    return {
+      user,
+      token
+    }
+  };
+};
+
+export default AuthenticateUserService;
