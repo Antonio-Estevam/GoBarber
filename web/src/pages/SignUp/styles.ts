026130f2204e6a19a493c0265c522c5aaa2767f0
antonio-estevam <jhonnyimmbe@gmail.com>
2021-11-23 15:05:15 -0300

    Criação pagina signUp e validação de dados

--- web/src/pages/SignUp/styles.ts
+++ web/src/pages/SignUp/styles.ts
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import signInBackgroundImg from '../../assets/sign-in-background.png';
+import signUpBackground from '../../assets/sign-up-background.png';
 import { shade } from 'polished';
 
 export const Container = styled.div`
@@ -39,7 +39,7 @@ export const Content = styled.div`
             }    
     }
    > a{
-    color: #ff9000;
+    color: #f4ede8;
     display: block;
     margin-top: 24px;
     text-decoration: none;
@@ -51,13 +51,13 @@ export const Content = styled.div`
         margin-right: 16px;
     }
     &:hover {
-        color: ${shade(0.2, '#ff9000')}
+        color: ${shade(0.2, '#f4ede8')}
     }
    }
 `;
 
 export const Background = styled.div`
     flex: 1;
-    background: url(${signInBackgroundImg}) no-repeat center;
+    background: url(${signUpBackground}) no-repeat center;
     background-size: cover;
 `;
